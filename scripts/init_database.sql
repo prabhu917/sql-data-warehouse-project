a3a5a47a9f7e6da1c8c4faccd6560ed2438215d4
prabhu917 <prabhuvarma368@gmail.com>
2025-05-22 11:00:19 -0500

    Update init_database.sql
    Added print statements

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -22,10 +22,16 @@ before running this script in any production or shared environment.
 
 
 --Using 'master' as the Database
+Print'=====================================';
+Print 'Utilizing the database "DataWarehouse"';
+Print'=====================================';
 USE master;
 GO
 
 --Drop the Database if it Exists
+Print'=====================================';
+Print 'Droping the database "DataWarehouse" if it alredy exists';
+Print'=====================================';
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 	BEGIN 
 		ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -34,19 +40,41 @@ IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 GO
 
 --creating the 'DataWarehouse' Database
+Print'=====================================';
+Print 'Creating OR Recreating the database "DataWarehouse"';
+Print'=====================================';
 CREATE DATABASE DataWarehouse;
 GO
 
 --Switching to 'Datawarehouse' as the Database
+Print'=====================================';
+Print 'Switching to database "DataWarehouse"';
+Print'=====================================';
 USE DataWarehouse;
 GO 
 
+Print'=====================================';
+Print 'Creating SCHEMAS';
+Print'=====================================';
+GO
 -- Creating Schemas for the layers Bronze, Silver, Gold
 CREATE SCHEMA bronze;
 GO
+Print'=====================================';
+Print 'Created SCHEMA Bronze';
+Print'=====================================';
+GO 
 
 CREATE SCHEMA silver;
 GO
+Print'=====================================';
+Print 'Created SCHEMA silver';
+Print'=====================================';
+GO
 
 CREATE SCHEMA gold;
+GO
+Print'=====================================';
+Print 'Created SCHEMA gold';
+Print'=====================================';
 GO
